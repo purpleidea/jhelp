8df7d3a5d8d7e9f3f55c12f01ee9bc52eefe1b59
James Shubin <purpleidea@gmail.com>
2010-02-23 11:20:41 -0500

    new files and updates

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Setup file for jhelp.
+
+This installer packages together a set of miscellaneous libraries that I have
+written or use often enough to warrant a dedicated repository to be shared by
+relevant software that uses these. They are packaged under the name of jhelp.
+"""
+# Copyright (C) 2008-2010  James Shubin, McGill University
+# Written for McGill University by James Shubin <purpleidea@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import distutils.core		#from distutils.core import setup, Extension
+import os
+
+
+# VARIABLES ###################################################################
+NAME = 'jhelp'
+data_files = []
+
+
+# DATA FILES ##################################################################
+data_files.append(('share/%s' % NAME, ['COPYING']))
+data_files.append(('share/%s' % NAME, ['COPYRIGHT']))
+data_files.append(('share/%s' % NAME, ['AUTHORS']))
+data_files.append(('share/%s' % NAME, ['VERSION']))
+
+
+# SETUP #######################################################################
+distutils.core.setup(
+	name=NAME,
+	# TODO: add misc package including get_version
+	#version=misc.get_version(),
+	packages=['jhelp'],
+	package_dir={'jhelp':'src'},
+	data_files=data_files		# the data files
+)
+
