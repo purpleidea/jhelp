4d03519a4b9cc8e6e2dab669c10c2d7fbcfd62db
James Shubin <purpleidea@gmail.com>
2010-02-23 15:56:12 -0500

    updated setup and gitignore. everything is more automatic now.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""Setup file for jhelp.
-
+"""Installer for jhelp.
+ 
 This installer packages together a set of miscellaneous libraries that I have
 written or use often enough to warrant a dedicated repository to be shared by
 relevant software that uses these. They are packaged under the name of jhelp.
@@ -22,13 +22,29 @@ relevant software that uses these. They are packaged under the name of jhelp.
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys			# for sys.modules
+import pydoc
 import distutils.core		#from distutils.core import setup, Extension
 import src.misc as misc		# i wrote this
+
+# VARIABLES ###################################################################
+description, ldescription = pydoc.splitdoc(pydoc.getdoc(sys.modules[__name__]))
 
 # SETUP #######################################################################
 distutils.core.setup(
 	name = 'jhelp',
 	version = misc.get_version(),
+	author='James Shubin',
+	author_email='purpleidea@gmail.com',
+	url='http://www.cs.mcgill.ca/~james/code/',
+	description=description,
+	long_description=ldescription,
+	# http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	classifiers=[
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: GNU Affero General Public License v3',
+		'Topic :: Software Development :: Libraries'
+	],
 	packages = ['jhelp'],
 	package_dir = {'jhelp':'src'},
 	data_files = misc.get_capitalized_files()
