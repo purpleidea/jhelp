850428e1ac78493b506969d2b72f48d5ce20f0c2
James Shubin <purpleidea@gmail.com>
2010-04-06 15:45:55 -0400

    Added prefix lib/function.

---
+++ src/prefix.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Find the prefix of the current installation.
+
+Finding the prefix that your program has been installed in can be non-trivial.
+This simplifies the process by allowing you to import <packagename>.prefix and
+get instant access to the path prefix by calling the function named: prefix().
+If you'd like to join this prefix onto a given path, pass it as the first arg.
+
+Example: if [ `./prefix.py` ]; then echo yes; else echo no; fi
+Example: x=`./prefix.py`; echo 'prefix: '$x
+"""
+# Copyright (C) 2009-2010  James Shubin, McGill University
+# Written for McGill University by James Shubin <purpleidea@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__all__ = ('prefix')
+#DEBUG = False
+
+import os
+import sys
+
+
+def prefix(join=None):
+	"""Returns the prefix that this code was installed into."""
+
+	# constants for this execution
+	path = os.path.abspath(__file__)
+	#if DEBUG: print 'path: %s' % path
+	name = os.path.basename(os.path.dirname(path))
+	#if DEBUG: print 'name: %s' % name
+	this = os.path.basename(path)
+	#if DEBUG: print 'this: %s' % this
+
+	# rule set
+	rules = [
+		# to match: /usr/lib/python2.5/site-packages/project/prefix.py
+		# or: /usr/local/lib/python2.6/dist-packages/project/prefix.py
+		lambda x: x == 'lib',
+		lambda x: x == ('python%s' % sys.version[:3]),
+		lambda x: x in ['site-packages', 'dist-packages'],
+		lambda x: x == name,	# 'project'
+		lambda x: x == this,	# 'prefix.py'
+	]
+
+	# matching engine
+	while len(rules) > 0:
+		(path, token) = os.path.split(path)
+		#if DEBUG: print 'path: %s, token: %s' % (path, token)
+		rule = rules.pop()
+		if not rule(token):
+			#if DEBUG: print 'rule failed'
+			return False
+
+	# usually returns: /usr/ or /usr/local/ (without slash postfix)
+	return path
+
+	if join is None:
+		return path
+	else:
+		return os.path.join(path, join)	# add on join if it exists!
+
+
+if __name__ == '__main__':
+	result = prefix()
+	if result:
+		print result
+	else:
+		sys.exit(1)
+
